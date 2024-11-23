0b75a2800429512c87f5713124fd57a69a7939db
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2024-11-22 23:58:17 -0600

    Update soft-refresh.js

--- dist/soft-refresh.js
+++ dist/soft-refresh.js
@@ -5,7 +5,7 @@ softRefresh(
     eruda.init()
     eruda.add(WebSocketMonitor.add())
   }, {
-    resources: [censorResource, 
+    resources: ["https://cdn.jsdelivr.net/gh/thatrandomperson5/censorjs@4945279/src/censor.js", 
                 "https://cdn.jsdelivr.net/npm/eruda", 
                 "https://cdn.gisthostfor.me/thatrandomperson5-lkuPg5hRm9-object-observer.min.js", 
                 "https://cdn.jsdelivr.net/gh/thatrandomperson5/eruda-websocketmonitor@master/src/websocketmonitor.js"]
