951866d9ad3debf0250338e318e00f00545284f3
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2024-11-22 22:25:58 -0600

    Create module.js

---
+++ dist/module.js
@@ -0,0 +1,3 @@
+import * as websocketmonitor from "https://cdn.jsdelivr.net/gh/thatrandomperson5/eruda-websocketmonitor@master/src/websocketmonitor.js"
+
+export {websocketmonitor.WebSocketMonitor as WebSocketMonitor}
