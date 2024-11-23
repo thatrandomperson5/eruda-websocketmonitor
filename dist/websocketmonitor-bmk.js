cf75fca91af509141d439864effcbd934711fb65
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2024-11-22 22:42:04 -0600

    Create websocketmonitor-bmk.js

---
+++ dist/websocketmonitor-bmk.js
@@ -0,0 +1,30 @@
+// For bookmarklet ++
+
+bookmarklet.config = {
+  packages: {
+    eruda: {
+      url: "https://cdn.jsdelivr.net/npm/eruda",
+      exports: ["eruda"]
+    },
+    censor: {
+      url: "https://cdn.jsdelivr.net/gh/thatrandomperson5/censorjs@4945279/src/censor.js",
+      exports: ["censor"]
+    },
+    objectObserver: {
+      url: "https://cdn.gisthostfor.me/thatrandomperson5-lkuPg5hRm9-object-observer.min.js",
+      exports: ["Observable"]
+    },
+    webSocketMonitor: {
+      url: "https://cdn.jsdelivr.net/gh/thatrandomperson5/eruda-websocketmonitor@master/src/websocketmonitor.js",
+      exports: ["WebSocketMonitor"]
+    }
+  }
+}
+
+bookmarklet.run = (ctx) => {
+  var eruda = ctx.packages.eruda.eruda
+  var WebSocketMonitor = ctx.packages.webSocketMonitor.WebSocketMonitor
+  
+  eruda.init()
+  eruda.add(WebSocketMonitor.add())
+}
